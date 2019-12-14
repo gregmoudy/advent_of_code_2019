62c8bca10568bf0ebf063344587ca848198e8918
SiN13 <gregmoudy@gmail.com>
2019-12-14 08:37:58 -0600

    Day 14 input parsing

--- day_14.py
+++ day_14.py
@@ -2,7 +2,27 @@ import os.path
 
 file_path = os.path.splitext( __file__ )[ 0 ] + '_input.txt'
 with open( file_path, 'r' ) as f:
-	lines = f.readlines( )
+	lines = f.read( ).splitlines( )
+
+	formulas = [ ]
+
+	for line in lines:
+		i, o, = line.split( '=>' )
+
+		o = o.lstrip( ' ' )
+		o = o.split( ' ' )
+		o = ( int( o[ 0 ] ), o[ 1 ] )
+
+		i = i.rstrip( ' ' )
+		i = i.split( ',' )
+		i = [ x.lstrip( ' ' ) for x in i ]
+		i = [ x.split( ' ' ) for x in i ]
+		i = [ ( int( x ), y ) for x, y in i ]
+
+		formulas.append( ( i, o ) )
+
+	# formulas ( [ (input num, input code) ], (output num, output code ) ),
+
 	print( 'break' )
 
 #======================================================================
